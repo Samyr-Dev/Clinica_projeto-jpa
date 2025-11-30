d317f595427fd942b40671167a13fc2a177e2d69
Samyr Tertuliano <samyrtertuliano4@gmail.com>
2025-11-30 19:28:26 -0300

    Ajustando layout de botões no Scene Builder

--- src/main/java/org/clinica/Main.java
+++ src/main/java/org/clinica/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader();
         Parent root = fxmlLoader.load(new FileInputStream(PAthFXML.PathFXML() + "\\MainView.fxml"));
-        Scene scene = new Scene(root, 800, 600);
+        Scene scene = new Scene(root, 700, 500);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
